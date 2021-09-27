52b501e11955ffa53acbfebd87ea5ad73c0a7a20
Mauro <maurogheco@gmail.com>
2021-09-27 14:12:09 -0300

    unicafe exercise 1.7, 1.8, 1.9, 1.10, 1.11 added

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,24 +2,66 @@ import React, { useState } from 'react';
 
 
 const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
-const Statistic = ({ text, count }) => <p>{text} {count}</p>
+
+const Statistics = (props) => {
+
+  const { good, neutral, bad, all } = props.value
+
+  if (all === 0) return <div><p>No feedback given</p></div>
+
+  return (
+    <table>
+      <StatisticLine text='good' value={good} />
+      <StatisticLine text='neutral' value={neutral} />
+      <StatisticLine text='bad' value={bad} />
+      <StatisticLine text='all' value={all} />
+      <StatisticLine text='average' value={(good - bad) / all} />
+      <StatisticLine text='positive' value={(good / all) * 100 + ' %'} />
+    </table>
+  )
+}
+
+const StatisticLine = ({ text, value }) => <tr><td>{text}</td><td>{value}</td></tr>
 
 const App = () => {
 
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [feedback, setFeedack] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    all: 0,
+  })
+
+  const handleClickGood = () => {
+    setFeedack({
+      ...feedback,
+      good: feedback.good + 1,
+      all: feedback.all + 1,
+    })
+  }
+  const handleClickNeutral = () => {
+    setFeedack({
+      ...feedback,
+      neutral: feedback.neutral + 1,
+      all: feedback.all + 1,
+    })
+  }
+  const handleClickBad = () => {
+    setFeedack({
+      ...feedback,
+      bad: feedback.bad + 1,
+      all: feedback.all + 1,
+    })
+  }
 
   return (
     <div>
       <h2>give feedback</h2>
-      <Button handleClick={() => setGood(good + 1)} text='good' />
-      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
-      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <Button handleClick={handleClickGood} text='good' />
+      <Button handleClick={handleClickNeutral} text='neutral' />
+      <Button handleClick={handleClickBad} text='bad' />
       <h2>statistics</h2>
-      <Statistic text='good' count={good} />
-      <Statistic text='neutral' count={neutral} />
-      <Statistic text='bad' count={bad} />
+      <Statistics value={feedback} />
     </div >
   )
 
