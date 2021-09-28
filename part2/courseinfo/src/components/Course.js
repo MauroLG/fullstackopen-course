f5fb59c04625e7756234d9887b1d2ccaf0f21e19
Mauro <maurogheco@gmail.com>
2021-09-28 18:40:02 -0300

    courseinfo exercise 2.1 added

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Header = ({ course }) => <h1>{course.name}</h1>
+
+const Content = ({ course }) => {
+  return (
+    <>
+      <Part part={course.parts[0]} />
+      <Part part={course.parts[1]} />
+      <Part part={course.parts[2]} />
+    </>
+  )
+}
+
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
+
+/* const Total = ({ course }) => <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p> */
+
+const Course = ({ course }) => {
+  console.log(course)
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      {/* <Total course={course} /> */}
+    </div>
+  )
+}
+
+export default Course
