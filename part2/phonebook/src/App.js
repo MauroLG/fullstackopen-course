3f0b80177282e11dad1f8a5cecec3e90a335faa1
Mauro <maurogheco@gmail.com>
2021-10-01 18:32:10 -0300

    phonebook exercise 2.11 added axios and db.json

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Person = ({ person }) => <li key={person.name}>{person.name} {person.number}</li>
+const Person = ({ person }) => <li>{person.name} {person.number}</li>
 
 const Persons = ({ filter, persons }) => {
   return (
     <ul>
-      {persons.filter(person => person.name.toLowerCase().includes(filter)).map(person => <Person person={person} />)}
+      {persons.filter(person => person.name.toLowerCase().includes(filter)).map(person => <Person key={person.name} person={person} />)}
     </ul>
   )
 }
